dc6c5bfee61f0614e8daac717d8cdad3288704e3
cryptomehedi <classicalmehedi@gmail.com>
2024-06-19 01:32:50 +0600

    server running add

--- index.js
+++ index.js
@@ -9,3 +9,14 @@ const port = process.env.PORT || 4000
 // mid ware 
 app.use(cors())
 app.use(express.json())
+
+
+
+
+app.get('/', (req, res) => {
+    res.send('Server Running')
+})
+
+app.all("*", (req, res) => {
+    res.send('No API Found')
+})
