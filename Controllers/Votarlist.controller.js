2908aadbed5325bac581ba49112d4b2f8352c5e5
cryptomehedi <classicalmehedi@gmail.com>
2024-06-19 23:11:41 +0600

    votarlist collection added

--- Controllers/Votarlist.controller.js
+++ Controllers/Votarlist.controller.js
@@ -1,6 +1,6 @@
 module.exports.getAllVotar = async (req, res, next) =>{
     try {
-        const votar = await votarCollection.find().toArray()
+        const votar = await VotarListsCollection.find().toArray()
             res.send(votar)
     } catch (error) {
         next(error)
--- Utils/DBCollection.js
+++ Utils/DBCollection.js
@@ -3,5 +3,8 @@ const { getDb } = require("./DBConnect")
 module.exports= {
     ServicesCollection : function(){
         return getDb().collection('listingsAndReviews')
+    },
+    VotarListsCollection : function () {
+        return getDb().collection('votarList')
     }
 }
