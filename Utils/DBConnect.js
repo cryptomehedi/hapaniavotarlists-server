970a35b58d0f3b4156887f5f8e89c4f55e57b019
cryptomehedi <classicalmehedi@gmail.com>
2024-06-20 03:46:18 +0600

    mongo connection fixed

--- Utils/DBConnect.js
+++ Utils/DBConnect.js
@@ -3,31 +3,35 @@ const {MongoClient, ServerApiVersion }= require("mongodb");
 
 const dbConnect = async ()=>{
     const  connectionString = process.env.ATLAS_URI;
-    return client = new MongoClient (connectionString, {
-        useNewUrlParser: true,
-        useUnifiedTopology: true,
-        serverApi: ServerApiVersion.v1
+    return client = new MongoClient(connectionString, {
+        // useNewUrlParser: true,
+        // useUnifiedTopology: true,
+        serverApi: {
+            version: ServerApiVersion.v1,
+            strict: true,
+            deprecationErrors: true,
+          },
+          
+        
+        // serverApi: ServerApiVersion.v1
     });
 }
 
 let dbConnection;
-
 module.exports = {
-    connectToServer: function (callback) { 
-        dbConnect()
-        client.connect(function (err, db) { 
-            if (err || !db) {
-                return callback(err);
-            }
-
-            dbConnection = db.db ("HapaniaVotarLists");
+    connectToServer: async function (callback) { 
+        await dbConnect()
+        try {
+            dbConnection = await client.db("HapaniaVotarLists");
             console.log("Successfully connected to MongoDB.");
-
             return callback();
-        });
+        } catch (error) {
+            return callback(error);
+        }
     },
 
     getDb: function () {
         return dbConnection;
     },
+    
 };
